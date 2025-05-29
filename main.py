23df9a0b7244a6b61965cabddd0512028be9cc10
Amier <ameirtech2020@gmail.com>
2025-05-29 13:17:59 +0200

    new api version

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ app.session_interface = CustomSessionInterface()
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # print_hi('PyCharm')
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5005, debug=True)
